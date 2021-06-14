0557b8f4b7faeba6c74dc7ff8701f1182edd370c
Jorge Luis Villarreal Rosales <83684831+jorgitolv98@users.noreply.github.com>
2021-06-13 19:08:24 -0500

    Update Villarreal_Jorge_DEBER_5.3.1_SE.ino

--- Villarreal_Jorge_DEBER_5.3.1_SE.ino
+++ Villarreal_Jorge_DEBER_5.3.1_SE.ino
@@ -9,73 +9,64 @@
  *      
  *           
  */
-#include <avr/power.h>
-#include <avr/sleep.h>
-#include <avr/wdt.h>
-#include <MsTimer2.h>
 
-int on;
-int tiempo = 0;
-int cont=0;
-
+#include <MsTimer2.h> // librería timer 2
+#include <avr/power.h> // librería power
+#include <avr/sleep.h> // librería sleep
+int on=0; // variable de encendido y modos del sistema
+int tiempo=0; // variable de selección de tiempo para toma de datos
+int cont=0; // conteo de timer 2
 void setup() {
-  Serial.begin(9600);
-  MsTimer2::set(1000,reloj);
-  attachInterrupt(0,activacion,LOW);
-  attachInterrupt(1,configuracion,LOW);
-
+Serial.begin(9600); //inicia cx serial
+MsTimer2::set(1000, reloj); // configura timer 2 a un segundo
+attachInterrupt(0,activacion,LOW);
+attachInterrupt(1,configuracion,LOW);
+power_adc_disable(); // deshabilidad adc
 }
-
 void loop() {
-  Serial.println(analogRead(0));
-  delay(1000);
-
+ Serial.println(analogRead(0)); // confirmación de desactivación del cad
+ delay(1000);
 }
-
-void activacion(){
-  switch(on){
-    case 0:
-    Serial.println("Inicio del sistema");
-    Serial.println("Seleccione tiempo de lectura del cad");
-    power_adc_disable();
-    on++;
-    break;
-    case 1:
-    Serial.println(String("Sitema funciona en:")+String(tiempo)+String("seg"));
-    MsTimer2::start();
-    on++;
-    break;
-    case 2:
-    Serial.println("Sistema apagado");
-    tiempo=0;
-    on=0;
-    MsTimer2::stop();
-    power_adc_disable();
-    break;
-  }
+void activacion (){ // rutina de interrupción 0
+switch(on){
+ case 0: // caso de activación
+ Serial.println("INICIO DEL SISTEMA");
+ Serial.println("SELECCIONE TIEMPO DE LECTURA DEL CAD");
+ on++;
+ break;
+ case 1: // caso de selección de tiempo
+ Serial.println(String("SISTEMA FUNCIONANADO EN: ")+String(tiempo
+)+String(" seg"));
+ MsTimer2::start(); // activa timer
+ on++;
+ break;
+ case 2: // sistema apagado
+ Serial.println("SISTEMA APAGADO");
+ tiempo=0;
+ on=0;
+ MsTimer2::stop(); // detiene timer
+ power_adc_disable(); // deshabilidad adc
+break;
+ }
 }
-
-
-void configuracion(){
-  if(on==1){
-    tiempo=tiempo+10;
-    if(tiempo<100){
-      Serial.println(String("Tiempo: ")+String(tiempo));
-    }else{
-      tiempo=0;
-    }
-  }
+void configuracion(){ // rutina interrupcion 1
+ if(on==1){
+ tiempo=tiempo+10;
+ if(tiempo<110){
+ Serial.println(String("TIEMPO: ")+String(tiempo));
+ }
+ else
+ tiempo=0;
+ }
 }
+void reloj(){ // rutina de timer 2
+ cont++; // cuenta cada segundo
+ Serial.println(String("Lectura en: ")+String(tiempo-cont)); // mensaje de tiempo
+ if(cont==tiempo){ // comparación con selección de usuario
+ power_adc_enable(); // habilita adc
+ delay(10);
+ Serial.println(analogRead(0)); // realiza la lectura
+ cont=0; // contador a cero
+ }
 
-void reloj(){
-  power_adc_disable();
-  cont++;
-  Serial.println(String("Lectura en:")+String(tiempo-cont));
-  if(cont==tiempo){
-    power_adc_enable();
-    delay(10);
-    Serial.println(analogRead(0));
-    cont=0;
-  
-  }
 }
